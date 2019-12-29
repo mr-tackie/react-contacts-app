5e5fc3fc2c209cd71636508cae87ac5197358b9a
Nii Abonie Tackie <abonious@gmail.com>
2019-12-29 16:39:12 +0000

    moved config variables to json file

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Auth0Provider } from "./react-auth0-spa";
+import auth0Config from './config.json';
 
 ReactDOM.render(
   <Auth0Provider
-    domain="nii.auth0.com"
-    client_id="5CKK0oQW6utNG7aQLiwDrBUyiE0tSck6"
+    domain={auth0Config.auth0_domain}
+    client_id={auth0Config.auth0_client_id}
     redirect_uri={window.location.origin}
   >
     <App />

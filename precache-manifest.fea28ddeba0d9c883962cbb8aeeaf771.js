24d8ae89fbd62579e3063f59d01bf6e998a66357
Nii Abonie Tackie <abonious@gmail.com>
2019-12-30 07:23:26 +0000

    Updates

---
+++ precache-manifest.fea28ddeba0d9c883962cbb8aeeaf771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cedca6f4e66b83fafbcda696cee83c2",
+    "url": "/react-contacts-app/index.html"
+  },
+  {
+    "revision": "a8088860490184e08bb4",
+    "url": "/react-contacts-app/static/css/2.c7b90ac4.chunk.css"
+  },
+  {
+    "revision": "fb51c72fc675b9805e93",
+    "url": "/react-contacts-app/static/css/main.078d6888.chunk.css"
+  },
+  {
+    "revision": "a8088860490184e08bb4",
+    "url": "/react-contacts-app/static/js/2.8f924481.chunk.js"
+  },
+  {
+    "revision": "fb51c72fc675b9805e93",
+    "url": "/react-contacts-app/static/js/main.95c79c67.chunk.js"
+  },
+  {
+    "revision": "e29b011e4e898d083100",
+    "url": "/react-contacts-app/static/js/runtime-main.9d0f65eb.js"
+  }
+]);
